a654ee3fbfd99661bccd95dad9696035343c7d59
Fernando Freitas <ylfernando3@gmail.com>
2025-08-04 14:23:30 -0300

    State Updater

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
 "use client";
-import { usuario } from "./components/usuario";
-import { students } from "@/data/students";
-import { StudentTable } from "./components/StudentTable";
+
 import { useState } from "react";
 
 export const Page = () => {
-  const [name, setName] = useState("");
+  const [count, setCount] = useState(0);
   const meuBotao = () => {
-    return alert(name);
+    setCount(count + 2);
+    setCount((c) => c + 2);
   };
 
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <input
-        type="text"
-        placeholder="digite seu nome"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      ></input>
-      <p>Seu nome é: {name}</p>
-
-      <button onClick={meuBotao}>Aperte aqui para enviar seu nome</button>
+      <p>{count}</p>
+      <button
+        className="bg-blue-700 text-white rounded-md p-4"
+        onClick={meuBotao}
+      >
+        +2
+      </button>
     </div>
   );
 };
